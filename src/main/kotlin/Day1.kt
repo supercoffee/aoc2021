b0aeef7e751f038eb9ef864bd9e53bf603d0e561
Benjamin Daschel <ben@lunarbyte.io>
2022-01-03 10:37:13 -0800

    day 1, part 2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -11,10 +11,20 @@ class Day1: Subcommand("day1", "Sonar sweep") {
         val lines = File(input).bufferedReader().readLines()
             .map { it.toInt() }
 
-        val increasedCount = lines.windowed(2, 1, true)
+        val noisyCount = lines.windowed(2, 1, true)
             .count {
                 it.size > 1 && (it[1] > it[0])
             }
-        println(increasedCount)
+
+        println("Noisy count $noisyCount")
+
+        val denoisedCount = lines.windowed(3, 1)
+            .map{ it.sum() }
+            .windowed(2, 1, true)
+            .count{
+                it.size > 1 && (it[1] > it[0])
+            }
+
+        println("Denoised count $denoisedCount")
     }
 }
